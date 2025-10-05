5d4c815c1e57020f01900b61bb6a9d19d63d7aa1
Jeniffer Hei <heihei990205@gmail.com>
2025-10-05 23:53:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3087748224-d33c9",
+  "appId": "1:420177324229:web:a1cf332a3e2ddb6a9328ac",
+  "apiKey": "AIzaSyDQae-ruGO-KbZ8P9k1rdy0CLriJUqVFvU",
+  "authDomain": "studio-3087748224-d33c9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "420177324229"
+};
