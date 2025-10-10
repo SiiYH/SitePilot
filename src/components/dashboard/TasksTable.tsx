b857f13d536b345a17585fd40f810b8c2e40927f
Jeniffer Hei <heihei990205@gmail.com>
2025-10-10 01:00:03 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/components/dashboard/TasksTable.tsx
+++ src/components/dashboard/TasksTable.tsx
@@ -1,6 +1,6 @@
 
 'use client';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { Task, User, UserRole } from '@/types';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
